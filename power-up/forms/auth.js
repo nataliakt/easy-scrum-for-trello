a473a3137667075c9a0134ff04d719bd194aedad
nataliakt <9091491+nataliakt@users.noreply.github.com>
2018-05-06 23:55:29 -0300

    Create auth.js

---
+++ power-up/forms/auth.js
@@ -0,0 +1,18 @@
+var t = window.TrelloPowerUp.iframe();
+
+t.render(function() {
+  return t.sizeTo('#content');
+})
+
+var oauthUrl = window.TrelloPowerUp.util.relativeUrl('auth/authorize.html');
+
+var authBtn = document.getElementById('authorize');
+authBtn.addEventListener('click', function() {
+  t.authorize(oauthUrl)
+  .then(function(token) {
+    return t.set('member', 'private', 'authToken', token)
+  })
+  .then(function() {
+    return t.closePopup();
+  });
+});

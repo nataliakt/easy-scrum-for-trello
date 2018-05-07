855b5d3003ffdb9d808b3825b66a12cbfe1204aa
nataliakt <9091491+nataliakt@users.noreply.github.com>
2018-05-06 22:05:33 -0300

    Update points.js

--- power-up/forms/points.js
+++ power-up/forms/points.js
@@ -3,36 +3,30 @@ var t = TrelloPowerUp.iframe();
 window.points.addEventListener('submit', function(event){
   event.preventDefault();
   
-  return t.set('card', 'shared', 'estimate', window.estimate.value)
+  return t.set('card', 'shared', {
+    'estimate': window.estimate.value,
+    'real': window.real.value,
+    'current': window.current.value
+  })
   .then(function(){
-    
-    return t.set('card', 'shared', 'real', window.real.value)
-    .then(function(){
-      
-      return t.set('card', 'shared', 'current', window.current.value)
-      .then(function() {
-        t.closePopup();
-      });
-    });
+    t.closePopup();
   });
 });
 
 t.render(function(){
-  return t.get('card', 'shared', 'estimate')
-  .then(function(estimate){
+  return t.get('card', 'shared')
+  .then(function(data){
+    var estimate = data.estimate;
+    var real = data.real;
+    var current = data.current;
+    
     window.estimate.value = estimate || '';
     
-    t.get('card', 'shared', 'real')
-    .then(function(real){
-      window.current.max = real || estimate || 0;
-      window.real.value = real || estimate || '';
-    });
+    window.current.max = real || estimate || 0;
+    window.real.value = real || estimate || '';
     
-    t.get('card', 'shared', 'current')
-    .then(function(current){
-      window.current.value = current || 0;
-      window.currentSpan.innerHTML = current || 0;
-    });
+    window.current.value = current || 0;
+    window.currentSpan.innerHTML = current || 0;
     
     window.estimate.oninput = function() {
       if (window.real.value == '') {

3f4f1bce9a120438b8f297b6342e6c988867022d
nataliakt <9091491+nataliakt@users.noreply.github.com>
2018-05-06 21:46:44 -0300

    Update points.js

--- power-up/forms/points.js
+++ power-up/forms/points.js
@@ -30,14 +30,18 @@ t.render(function(){
     });
     
     window.estimate.oninput = function() {
-      if (window.real.value <= window.estimate.value) {
+      if (window.real.value < window.estimate.value) {
         window.current.max = this.value;
+      } else {
+        window.current.max = window.real.value;
       }
     }
     
     window.real.oninput = function() {
       if (window.real.value > window.estimate.value) {
         window.current.max = this.value;
+      } else {
+        window.current.max = window.estimate.value;
       }
     }
     
